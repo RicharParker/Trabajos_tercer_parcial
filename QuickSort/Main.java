c7e4e8d70c356d47b2677579ae765cdadf024e77
RicharParker <61020823+RicharParker@users.noreply.github.com>
2020-11-23 22:38:16 -0600

    Update Main.java

--- QuickSort/Main.java
+++ QuickSort/Main.java
@@ -9,11 +9,11 @@ public class Main {
     public static void main(String[] args){
         
         int i=0, cantidad=1000000, rango=1000000;
-        int arreglo[] = new int[cantidad];//el arreglo se va a generar de la cantidad que estamos colocando
+        int arreglo[] = new int[cantidad];
         
-        arreglo[i]=(int)(Math.random()*rango);//vamos a almacenar en la localidad 0 de nuestro arreglo el 1er num aleatorio que estemos generando. dependiendo del rango
+        arreglo[i]=(int)(Math.random()*rango);
         for(i=1; i<cantidad; i++){
-            arreglo[i]=(int)(Math.random()*rango);//le damos otro valor a otra localidad arreglo
+            arreglo[i]=(int)(Math.random()*rango);
             for(int j=0; j<i; j++){
                 if(arreglo[i]==arreglo[j]){
                     i--;//
